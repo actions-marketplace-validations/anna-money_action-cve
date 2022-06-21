b9bd0bcf29260322f6ced74cfcc13d90b7825e0c
Ruslan Yaparov <rayaparov@gmail.com>
2022-06-21 16:51:44 +0400

    use previous icon

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const ACTION_ICON =
-  'https://storage.googleapis.com/anna-ws-prod/images/anna-logo.png'
+  'https://github.com/kunalnagarco/action-cve/raw/main/icons/ladybug.png'
 
 export const ACTION_SHORT_SUMMARY = 'GitHub Action - Security alerts'
 
